ed01e9e20e3d392a1d175215ae5b1ffdcffab63d
C <c.van.der.deen@student.rug.nl>
2025-04-30 11:00:20 +0200

    feature extract experimentation

--- perform_svm.py
+++ perform_svm.py
@@ -27,6 +27,28 @@ def main():
     eval_matrix = metrics.confusion_matrix(y_true=te_labels, y_pred=pre_labels)
     print(eval_matrix)
 
+    list_words = []
+    for word in transformers[0].vocabulary_:
+        list_words.append(word)
+    print(list_words)
+    exit()
+
+    list_weights = []
+    for coef in cls.coef_:
+        for a in coef:
+            list_weights.append(a)
+    
+    list_wordweights = []
+    for word, weight in zip(list_words, list_weights):
+        list_wordweights.append([weight, word])
+    
+    list_wordweights.sort()
+    print(list_wordweights[-10:])
+    
+    for item in list_wordweights:
+        if item[1] == "heappop":
+            print(item[0])
+
 
 if __name__ == "__main__":
     main()
