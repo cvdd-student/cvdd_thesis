9eed1b342191682d7ed4183e960a407ed692bb1e
cvdd-student <c.van.der.deen@student.rug.nl>
2025-06-04 17:48:33 +0200

    2022 eval

--- perform_logreg.py
+++ perform_logreg.py
@@ -55,6 +55,42 @@ def main():
     # Create and show evaluation matrix
     eval_matrix = metrics.confusion_matrix(y_true=te_labels, y_pred=pre_labels)
     print(eval_matrix)
+    
+    # OUT OF SCOPE Data
+    print("\n##### 2022 DATA #####\n")
+    eval_2022_feats, eval_2022_labels = organise_data.make_2022_feats(transformers)
+    pre_2022_predictions = clf.predict_proba(eval_2022_feats)
+    eval_2022_baseline_labels = []
+    for i in range(len(eval_2022_labels)):
+        eval_2022_baseline_labels.append("human")
+    
+    # Create OUT OF SCOPE labels from the probabilities to classify
+    pre_2022_labels = []
+    for prediction in pre_2022_predictions:
+        if prediction[0] > 0.5:
+            pre_2022_labels.append("gemini")
+        else:
+            pre_2022_labels.append("human")
+    
+    # OUT OF SCOPE metrics
+    eval_2022_accuracy_baseline = metrics.accuracy_score(y_true=eval_2022_labels, y_pred=eval_2022_baseline_labels)
+    eval_2022_precision_baseline = metrics.precision_score(y_true=eval_2022_labels, y_pred=eval_2022_baseline_labels, average='macro', zero_division=0.0)
+    eval_2022_recall_baseline = metrics.recall_score(y_true=eval_2022_labels, y_pred=eval_2022_baseline_labels, average='macro')
+    eval_2022_f1score_baseline = metrics.f1_score(y_true=eval_2022_labels, y_pred=eval_2022_baseline_labels, average='macro', zero_division=0.0)
+    
+    eval_2022_accuracy = metrics.accuracy_score(y_true=eval_2022_labels, y_pred=pre_2022_labels)
+    eval_2022_precision = metrics.precision_score(y_true=eval_2022_labels, y_pred=pre_2022_labels, average='macro', zero_division=0.0)
+    eval_2022_recall = metrics.recall_score(y_true=eval_2022_labels, y_pred=pre_2022_labels, average='macro')
+    eval_2022_f1score = metrics.f1_score(y_true=eval_2022_labels, y_pred=pre_2022_labels, average='macro', zero_division=0.0)
+
+    print("Accuracy: " + str(eval_2022_accuracy) + " (Baseline: " + str(eval_2022_accuracy_baseline) + ")")
+    print("Precision: " + str(eval_2022_precision)  + " (Baseline: " + str(eval_2022_precision_baseline) + ")")
+    print("Recall: " + str(eval_2022_recall)  + " (Baseline: " + str(eval_2022_recall_baseline) + ")")
+    print("F1: " + str(eval_2022_f1score)  + " (Baseline: " + str(eval_2022_f1score_baseline) + ")")
+
+    # Create and show OUT OF SCOPE evaluation matrix
+    eval_matrix = metrics.confusion_matrix(y_true=eval_2022_labels, y_pred=pre_2022_labels)
+    print(eval_matrix)
 
 
 if __name__ == "__main__":
--- perform_svm.py
+++ perform_svm.py
@@ -52,9 +52,29 @@ def main():
     eval_matrix = metrics.confusion_matrix(y_true=te_labels, y_pred=pre_labels)
     print(eval_matrix)
     
+    # OUT OF SCOPE Data
+    print("\n##### 2022 DATA #####\n")
     eval_2022_feats, eval_2022_labels = organise_data.make_2022_feats(transformers)
-    
     pre_2022_labels = cls.predict(eval_2022_feats)
+    eval_2022_baseline_labels = []
+    for i in range(len(eval_2022_labels)):
+        eval_2022_baseline_labels.append("human")
+    
+    # OUT OF SCOPE metrics
+    eval_2022_accuracy_baseline = metrics.accuracy_score(y_true=eval_2022_labels, y_pred=eval_2022_baseline_labels)
+    eval_2022_precision_baseline = metrics.precision_score(y_true=eval_2022_labels, y_pred=eval_2022_baseline_labels, average='macro', zero_division=0.0)
+    eval_2022_recall_baseline = metrics.recall_score(y_true=eval_2022_labels, y_pred=eval_2022_baseline_labels, average='macro')
+    eval_2022_f1score_baseline = metrics.f1_score(y_true=eval_2022_labels, y_pred=eval_2022_baseline_labels, average='macro', zero_division=0.0)
+    
+    eval_2022_accuracy = metrics.accuracy_score(y_true=eval_2022_labels, y_pred=pre_2022_labels)
+    eval_2022_precision = metrics.precision_score(y_true=eval_2022_labels, y_pred=pre_2022_labels, average='macro', zero_division=0.0)
+    eval_2022_recall = metrics.recall_score(y_true=eval_2022_labels, y_pred=pre_2022_labels, average='macro')
+    eval_2022_f1score = metrics.f1_score(y_true=eval_2022_labels, y_pred=pre_2022_labels, average='macro', zero_division=0.0)
+
+    print("Accuracy: " + str(eval_2022_accuracy) + " (Baseline: " + str(eval_2022_accuracy_baseline) + ")")
+    print("Precision: " + str(eval_2022_precision)  + " (Baseline: " + str(eval_2022_precision_baseline) + ")")
+    print("Recall: " + str(eval_2022_recall)  + " (Baseline: " + str(eval_2022_recall_baseline) + ")")
+    print("F1: " + str(eval_2022_f1score)  + " (Baseline: " + str(eval_2022_f1score_baseline) + ")")
     
     # Create and show evaluation matrix
     eval_matrix = metrics.confusion_matrix(y_true=eval_2022_labels, y_pred=pre_2022_labels)
